fa88b6bc3ce59eee0bffdc643d7a53e13ffdd130
Rafaela Villatore <morango20n1@gmail.com>
2025-10-22 07:34:24 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,15 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const database = new DatabasePostgres();
 
 //cadastro
-app.post('/auth/register', async (req, res) => {
-    const {name, email, password} = req.body;
-    if (!name || !email || !password){
-        return res.status(400).json({msg: 'Preencha todos os campos!'  });
+app.post('/auth/register', async(req, res) => {
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({ msg: 'Preencha todos os campos!' });
     }
 
     const existingUser = await database.findByEmail(email);
@@ -29,10 +30,80 @@ app.post('/auth/register', async (req, res) => {
     res.status(201).json({ msg: 'Usuário criado!'});
 })
 
-//Login
+//login
 app.post('/auth/login', async(req, res) => {
     const { email, password } = req.body;
     if(!email || !password){
-        return res.status(400).json({ msg: 'Preencha o email e senha'});
+        return res.status(400).json({ msg: 'Preencha email e senha!'});
     }
-})
+
+    const user = await database.findByEmail(email);
+    if(!user){
+        return res.status(400).json({ msg: 'Usuário não encontrado'})
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+        return res.status(401).json({ msg: 'Senha inválida!' });
+    }
+
+    const token = jwt.sing(
+        { id: user.id, email: user.email},
+        process.env.JWT_SECRET || 'minhaChaveSuperSecreta',
+        { expiresId: 'id'}
+    );
+
+    res.json({
+        msg: 'Login realizado!',
+        token,
+        user: { id: user.id, name: user.name, email: user.email}
+    });
+});
+
+//Teste de rota protegida
+app.get('/protected', (req, res) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) return res.status(401).json({ msg: 'Token não fornecido!' });
+
+    const token = authHeader.split(' ')[1];
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'minhaChaveSuperSecreta');
+        res.json({ msg: 'Acesso autorizado! ', decoded});
+    }catch(err){
+        res.status(401).json({ msg: 'Token inválido!'});
+    }
+});
+
+//GET /users
+app.get('/users', async(req, res) =>{
+    const users = await database.list();
+    res.json(users);
+});
+
+//POST /users
+app.post('/users', async(req, res) => {
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({ msg: 'Preencha todos os campos!' });
+    }
+    await database.create({ name, email, password });
+    res.status(201).send();
+});
+
+//PUT /users/:id
+app.put('/users/:id', async(req, res) => {
+    const id = req.params.id;
+    const user = req.body;
+    await database.update(id, user);
+    res.status(204).send();
+});
+
+app.delete('/users/:id', async(req, res) => {
+    const id = req.params.id;
+    await database.delete(id);
+    res.status(204).send();
+});
+
+app.listen(3001, () => {
+    console.log('Servidor rodando na porta 3001');
+});
