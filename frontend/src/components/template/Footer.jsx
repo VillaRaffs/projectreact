3613c870659fae2902602052b7caa756ab16e0df
Rafaela Villatore <morango20n1@gmail.com>
2025-11-04 09:37:57 -0300

    Add files via upload

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -1,2 +1,12 @@
 import './Footer.css';
 import React from 'react';
+
+const footer = () => (
+    <footer className="footer">
+        <span>
+            Desenvolvido por Rafaela
+        </span>
+        </footer>
+);
+
+export default Footer;
