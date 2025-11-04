6c2b7fc85d72dba8732a0aabdb5090ecbd9abc7b
Rafaela Villatore <morango20n1@gmail.com>
2025-11-04 07:56:47 -0300

    Add files via upload

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+import Main from "../template/Main";
+
+const Home = () => (
+    <Main icon="home" title="Início" subtitle="Primeiro Projeto do capítulo de React.">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção de um projeto desenvolvimento em React!</p>
+    
+    </Main>
+);
+
+export default Home;
